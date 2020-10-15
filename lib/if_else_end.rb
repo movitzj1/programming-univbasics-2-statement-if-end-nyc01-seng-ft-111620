b63610d44edff24dd8e7ab5d3cadaccdd18c87f6
Justin Movitz <movitzj1@montclair.edu>
2020-10-15 16:57:25 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,6 +1,6 @@
-run_code_inside = false
-puts "Code before if...end"
-if run_code_inside
-  puts "code inside"
-end
-puts "Code after if...end"
+current_time = Time.now
+current_time = current_time.to_i
+if current_time % == 0
+  puts "Even!"
+else
+  puts "Odd!"
